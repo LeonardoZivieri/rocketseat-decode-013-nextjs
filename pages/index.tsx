ea9c03cd1fb04e50ae917c5fb70b6842cb0e2b8c
FunctionOne <leonardozivieri150@gmail.com>
2022-05-28 13:47:05 -0300

    Example of CSR ( Client Side Rendering )

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [repositories, setRepositories] = useState([] as string[]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/LeonardoZivieri/repos')
+      .then(d => d.json())
+      .then(repositories => {
+        const repositoryNames = repositories.map((repo) => repo.name);
+        setRepositories(repositoryNames)
+      })
+  }, [])
+
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>Hello World!</h1>
+      <h3>I'm Leonardo and these are my repos:</h3>
+      <ul>
+        {repositories.map((repo) => <li key={repo}>{repo}</li>)}
+      </ul>
+    </>
   )
 }
